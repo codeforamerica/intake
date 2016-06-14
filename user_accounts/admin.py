2d8924510c94cef20011883f2993846a61e6b58d
Ben Golder <bgolder@codeforamerica.org>
2016-06-14 10:59:52 -0700

    add user profile to admin

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -4,3 +4,7 @@ from . import models
 @admin.register(models.Organization)
 class OrganizationAdmin(admin.ModelAdmin):
     pass
+
+@admin.register(models.UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
