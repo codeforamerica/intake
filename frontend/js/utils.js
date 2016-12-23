4bd81ac945ff70e2bab27b443cef649d400e3a03
Ben Golder <bgolder@codeforamerica.org>
2016-12-22 18:56:53 -0800

    add a way to pull out a cookie in the frontend

--- frontend/js/utils.js
+++ frontend/js/utils.js
@@ -18,5 +18,20 @@ module.exports = {
         for (var attrName in obj2) {
             obj1[attrName] = obj2[attrName];
         }
+    },
+    getCookie: function(name) {
+      var cookieValue = null;
+      if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].trim();
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            break;
+          }
+        }
+      }
+      return cookieValue;
     }
 }
