cb6182e05dbbb270bac81c2b8cbca9ce802f6e0e
Ben Golder <bgolder@codeforamerica.org>
2016-12-22 18:57:19 -0800

    add csrf support for ajax requests

---
+++ frontend/js/csrf.js
@@ -0,0 +1,20 @@
+
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+function setupAjaxCsrf(jQueryObject, csrftoken) {
+ jQueryObject.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+  });
+
+}
+
+module.exports = {
+  setupAjaxCsrf: setupAjaxCsrf
+};
