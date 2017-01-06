4d75e78dc51831a107e5401126d691a88ce0e761
Ben Golder <bgolder@codeforamerica.org>
2017-01-05 22:45:59 -0800

    added organization info to constants, footer

--- intake/constants.py
+++ intake/constants.py
@@ -40,17 +40,33 @@ GENDER_PRONOUN_CHOICES = (
     ('none', _('Prefer not to answer')),
 )
 
+REASON_FOR_APPLYING_CHOICES = (
+    ('background_check', _('To pass a background check')),
+    ('pending_job', _('I have a job offer pending')),
+    ('housing', _('To qualify for housing')),
+    ('immigration', _('Immigration status issues')),
+    ('lost_job', _('Was asked to leave job because of my record')),
+    ('early_probation', _('End probation early')),
+    ('homeless', _('Homeless or on social services')),
+    ('other', _('My reason is not listed')),
+)
+
 STAFF_NAME_CHOICES = ['Jazmyn', 'Ben']
 
 
 class Organizations:
+    ALL = 'all'
     CFA = 'cfa'
     SF_PUBDEF = 'sf_pubdef'
     COCO_PUBDEF = 'cc_pubdef'
     ALAMEDA_PUBDEF = 'a_pubdef'
     MONTEREY_PUBDEF = 'monterey_pubdef'
     EBCLC = 'ebclc'
-    ALL = 'all'
+    SOLANO_PUBDEF = 'solano_pubdef'
+    SAN_DIEGO_PUBDEF = 'san_diego_pubdef'
+    SAN_JOAQUIN_PUBDEF = 'san_joaquin_pubdef'
+    SANTA_CLARA_PUBDEF = 'santa_clara_pubdef'
+    FRESNO_PUBDEF = 'fresno_pubdef'
 
 DEFAULT_ORGANIZATION_ORDER = [
     Organizations.ALL,
@@ -59,6 +75,11 @@ DEFAULT_ORGANIZATION_ORDER = [
     Organizations.EBCLC,
     Organizations.COCO_PUBDEF,
     Organizations.MONTEREY_PUBDEF,
+    Organizations.SOLANO_PUBDEF,
+    Organizations.SAN_DIEGO_PUBDEF,
+    Organizations.SAN_JOAQUIN_PUBDEF,
+    Organizations.SANTA_CLARA_PUBDEF,
+    Organizations.FRESNO_PUBDEF,
 ]
 
 
@@ -69,6 +90,11 @@ ORG_NAMES = {
     Organizations.ALAMEDA_PUBDEF: _("Alameda County Public Defender's Office"),
     Organizations.MONTEREY_PUBDEF: _("Monterey County Public Defender"),
     Organizations.EBCLC: _("East Bay Community Law Center"),
+    Organizations.SOLANO_PUBDEF: _("Solano County Public Defender"),
+    Organizations.SAN_DIEGO_PUBDEF: _("San Diego County Public Defender"),
+    Organizations.SAN_JOAQUIN_PUBDEF: _("San Joaquin County Public Defender"),
+    Organizations.SANTA_CLARA_PUBDEF: _("Santa Clara County Public Defender"),
+    Organizations.FRESNO_PUBDEF: _("Fresno County Public Defender"),
 }
 
 PAGE_COMPLETE_SEQUENCES = {
@@ -81,6 +107,13 @@ PAGE_COMPLETE_SEQUENCES = {
         "SelectCounty", "CountyApplication",
         "DeclarationLetterView", "DeclarationLetterReviewPage"],
     Organizations.EBCLC: ["SelectCounty", "CountyApplication"],
+    Organizations.SOLANO_PUBDEF: ["SelectCounty", "CountyApplication"],
+    Organizations.SAN_DIEGO_PUBDEF: ["SelectCounty", "CountyApplication"],
+    Organizations.SAN_JOAQUIN_PUBDEF: ["SelectCounty", "CountyApplication"],
+    Organizations.SANTA_CLARA_PUBDEF: [
+        "SelectCounty", "CountyApplication",
+        "DeclarationLetterView", "DeclarationLetterReviewPage"],
+    Organizations.FRESNO_PUBDEF: ["SelectCounty", "CountyApplication"],
 }
 
 
@@ -89,6 +122,11 @@ class Counties:
     SAN_FRANCISCO = 'sanfrancisco'
     ALAMEDA = 'alameda'
     MONTEREY = 'monterey'
+    SOLANO = 'solano'
+    SAN_DIEGO = 'san_diego'
+    SAN_JOAQUIN = 'san_joaquin'
+    SANTA_CLARA = 'santa_clara'
+    FRESNO = 'fresno'
     OTHER = 'other'
 
 
@@ -97,40 +135,51 @@ class CountyNames:
     CONTRA_COSTA = 'Contra Costa'
     ALAMEDA = 'Alameda'
     MONTEREY = 'Monterey'
-    ALL = 'San Francisco, Alameda, Contra Costa, and Monterey Counties'
+    SOLANO = 'Solano'
+    SAN_DIEGO = 'San Diego'
+    SAN_JOAQUIN = 'San Joaquin'
+    SANTA_CLARA = 'Santa Clara'
+    FRESNO = 'Fresno'
+    ALL = str(
+        'San Francisco, Alameda, Contra Costa, Solano, Santa Clara, '
+        'Fresno, San Joaquin, Monterey, and San Diego Counties')
 
 
 if SCOPE_TO_LIVE_COUNTIES:
-    CountyNames.ALL = 'San Francisco, Alameda, and Contra Costa Counties'
+    CountyNames.ALL = str(
+        'San Francisco, Alameda, Contra Costa, and Monterey Counties')
 
 
-if SCOPE_TO_LIVE_COUNTIES:
-    COUNTY_CHOICES = (
-        (Counties.SAN_FRANCISCO, _('San Francisco')),
-        (Counties.CONTRA_COSTA, _(
-            'Conta Costa County (near Richmond, Concord, Walnut Creek, '
-            'San Ramon, Antioch, or Brentwood)')),
-        (Counties.ALAMEDA, _(
-            'Alameda County (near Oakland, Berkeley, San Leandro, Hayward, '
-            'Fremont, Albany, Newark, Dublin, Union City, Pleasanton, '
-            'or Livermore)')),
-        # (Counties.MONTEREY, _(
-        #     'Monterey County (near Salinas, Monterey, Marina, Seaside, '
-        #     'Prunedale, Castroville, or King City)')),
-    )
-else:
-    COUNTY_CHOICES = (
-        (Counties.SAN_FRANCISCO, _('San Francisco')),
-        (Counties.CONTRA_COSTA, _(
-            'Conta Costa County (near Richmond, Concord, Walnut Creek, '
-            'San Ramon, Antioch, or Brentwood)')),
-        (Counties.ALAMEDA, _(
-            'Alameda County (near Oakland, Berkeley, San Leandro, Hayward, '
-            'Fremont, Albany, Newark, Dublin, Union City, Pleasanton, '
-            'or Livermore)')),
-        (Counties.MONTEREY, _(
-            'Monterey County (near Salinas, Monterey, Marina, Seaside, '
-            'Prunedale, Castroville, or King City)')),
+COUNTY_CHOICES = (
+    (Counties.SAN_FRANCISCO, _('San Francisco')),
+    (Counties.CONTRA_COSTA, _(
+        'Conta Costa County (near Richmond, Concord, Walnut Creek, '
+        'San Ramon, Antioch, or Brentwood)')),
+    (Counties.ALAMEDA, _(
+        'Alameda County (near Oakland, Berkeley, San Leandro, Hayward, '
+        'Fremont, Albany, Newark, Dublin, Union City, Pleasanton, '
+        'or Livermore)')),
+    (Counties.MONTEREY, _(
+        'Monterey County (near Salinas, Monterey, Marina, Seaside, '
+        'Prunedale, Castroville, or King City)')),
+)
+if not SCOPE_TO_LIVE_COUNTIES:
+    COUNTY_CHOICES += (
+        (Counties.SOLANO, _(
+            'Solano County (near Vallejo, Fairfield, Vacaville, Benicia, '
+            'or Allendale)')),
+        (Counties.SAN_DIEGO, _(
+            'San Diego County (near San Diego, Oceanside, Chula Vista, or '
+            'Escondido)')),
+        (Counties.SAN_JOAQUIN, _(
+            'San Joaquin County (near Lodi, Stockton, Tracy, Manteca, '
+            'Thornton, or Victor)')),
+        (Counties.SANTA_CLARA, _(
+            'Santa Clara County (near San Jose, Santa Clara, Campbell, '
+            'Saratoga, Los Altos, Los Gatos, or Gilroy)')),
+        (Counties.FRESNO, _(
+            'Fresno County (near Fresno, Clovis, Sanger, Kingsburg, Mendota, '
+            'Fowler, or Three Rocks)')),
     )
 
 COUNTY_CHOICE_DISPLAY_DICT = {
@@ -138,6 +187,11 @@ COUNTY_CHOICE_DISPLAY_DICT = {
     Counties.CONTRA_COSTA: CountyNames.CONTRA_COSTA,
     Counties.ALAMEDA: CountyNames.ALAMEDA,
     Counties.MONTEREY: CountyNames.MONTEREY,
+    Counties.SOLANO: CountyNames.SOLANO,
+    Counties.SAN_DIEGO: CountyNames.SAN_DIEGO,
+    Counties.SAN_JOAQUIN: CountyNames.SAN_JOAQUIN,
+    Counties.SANTA_CLARA: CountyNames.SANTA_CLARA,
+    Counties.FRESNO: CountyNames.FRESNO,
 }
 
 
