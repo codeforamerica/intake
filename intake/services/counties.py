1caad30c9245a8fb8a3bbd96769f7c73b2ebc378
Ben Golder <bgolder@codeforamerica.org>
2017-01-25 10:36:22 -0800

    added counties services w tests

---
+++ intake/services/counties.py
@@ -0,0 +1,8 @@
+from user_accounts.models import Organization
+from intake.models import County
+
+
+def get_live_counties_and_orgs():
+    live_orgs = Organization.objects.filter(is_live=True)
+    live_counties = County.objects.filter(organizations__in=live_orgs)
+    return live_counties, live_orgs
