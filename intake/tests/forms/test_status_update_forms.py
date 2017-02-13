58c436918cea8e07f83219bd06c583256eb6661c
Ben Golder <bgolder@codeforamerica.org>
2017-02-13 10:08:46 -0800

    make sure nonactive template options are excluded

--- intake/tests/forms/test_status_update_forms.py
+++ intake/tests/forms/test_status_update_forms.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
+from intake import models
 from intake.forms import StatusUpdateForm, StatusNotificationForm
+from markupsafe import escape
 
 
 class TestStatusUpdateForm(TestCase):
+
+    fixtures = ['template_options']
+
     def test_has_expected_fields(self):
         keys = [
             'author', 'application', 'status_type',
@@ -10,8 +15,16 @@ class TestStatusUpdateForm(TestCase):
         form = StatusUpdateForm()
         self.assertEquals(keys, list(form.fields.keys()))
 
-    def test_non_live_next_steps_arent_shown(self):
-        raise NotImplementedError
+    def test_inactive_next_steps_arent_shown(self):
+        next_step = models.NextStep.objects.last()
+        next_step.is_active = False
+        next_step.save()
+        form = StatusUpdateForm()
+        rendered_form = form.as_p()
+        self.assertNotIn(
+            escape(next_step.label),
+            rendered_form
+        )
 
 
 class TestStatusNotificationForm(TestCase):
