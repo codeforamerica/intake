2568ef663b6f45d3bf946c39dbd167d106ed489c
Ben Golder <bgolder@codeforamerica.org>
2017-02-17 12:11:11 -0800

    add doc string

--- intake/services/statistics.py
+++ intake/services/statistics.py
@@ -9,18 +9,40 @@ ALL = (constants.Organizations.ALL, 'Total (All Organizations)')
 
 
 def get_status_update_success_metrics():
-    """
-    Returns the total and statustype totals of status updates and notifications
-        total & by org
-        'updates' {
-            'all': Counter(
-                'Total': 23,
-                'No Convictions': 4,
-                'Eligible': 6,
+    """calculates the total number of status updates,
+        including the totals by type of status for all orgs and each org.
 
+    Example output:
 
-            }
-        }
+    [('Total (All Organizations)',
+      [('Total', 20),
+       ('Eligible', 3),
+       ('Passed', 3),
+       ('No Convictions', 3),
+       ('Court Date', 3),
+       ("Can't Proceed", 3),
+       ('Not Eligible', 3),
+       ('Declined', 2)]),
+     ('San Francisco Public Defender',
+      [('Total', 2), ('Declined', 1), ("Can't Proceed", 1)]),
+     ("Alameda County Public Defender's Office",
+      [('Total', 2), ('Eligible', 1), ('Not Eligible', 1)]),
+     ('East Bay Community Law Center',
+      [('Total', 2), ('Passed', 1), ('Court Date', 1)]),
+     ('Contra Costa Public Defender',
+      [('Total', 2), ("Can't Proceed", 1), ('No Convictions', 1)]),
+     ('Monterey County Public Defender',
+      [('Total', 2), ('No Convictions', 1), ('Not Eligible', 1)]),
+     ('Solano County Public Defender',
+      [('Total', 2), ('Eligible', 1), ('Court Date', 1)]),
+     ('San Diego County Public Defender',
+      [('Total', 2), ('Passed', 1), ('Declined', 1)]),
+     ('San Joaquin County Public Defender',
+      [('Total', 2), ("Can't Proceed", 1), ('No Convictions', 1)]),
+     ('Santa Clara County Public Defender',
+      [('Total', 2), ('Eligible', 1), ('Not Eligible', 1)]),
+     ('Fresno County Public Defender',
+      [('Total', 2), ('Passed', 1), ('Court Date', 1)])]
     """
     updates = models.StatusUpdate.objects.all().prefetch_related(
         'application__organization',
