a605440fd61dca14741cfac197fc3d9a5b856881
Ben Golder <bgolder@codeforamerica.org>
2017-02-23 14:32:52 -0800

    show hacked suboptimal way to handle celery tasks

--- intake/tasks.py
+++ intake/tasks.py
@@ -1,5 +1,25 @@
 from celery import shared_task
-from project.external_services import log_to_mixpanel
+from django.conf import settings
+
+from mixpanel import Mixpanel
+
+_mixpanel_client = None
 
 
-log_to_mixpanel = shared_task(log_to_mixpanel)
+def get_mixpanel_client():
+    global _mixpanel_client
+    if _mixpanel_client is None:
+        mixpanel_key = getattr(settings, 'MIXPANEL_KEY', None)
+        if mixpanel_key:
+            _mixpanel_client = Mixpanel(mixpanel_key)
+    return _mixpanel_client
+
+
+@shared_task
+def mixpanel_task(distinct_id, event_name, data):
+    client = get_mixpanel_client()
+    if client:
+        client.track(
+            distinct_id=distinct_id,
+            event_name=event_name,
+            properties=data)
