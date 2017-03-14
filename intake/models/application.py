612dcff1d9b4237a1dc93093b9f9827e392ab6d0
Ben Golder <bgolder@codeforamerica.org>
2017-03-13 18:16:22 -0700

    add created, updated, was_transferred fields to Application

--- intake/models/application.py
+++ intake/models/application.py
@@ -1,7 +1,8 @@
 from django.db import models
+from .abstract_base_models import BaseModel
 
 
-class Application(models.Model):
+class Application(BaseModel):
     organization = models.ForeignKey(
         'user_accounts.Organization',
         on_delete=models.PROTECT,
@@ -12,8 +13,11 @@ class Application(models.Model):
         on_delete=models.PROTECT,
         related_name='applications'
     )
+    was_transferred_out = models.BooleanField(default=False)
 
     def __str__(self):
-        sub = str(self.form_submission.id)
-        org = self.organization.name
-        return sub + org
+        return "Sub {} ({}) to {} on {}".format(
+            self.form_submission.id,
+            self.form_submission.anonymous_name,
+            self.organization.slug,
+            self.form_submission.get_local_date_received('%Y-%m-%d'))
