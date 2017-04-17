4ed848bc0510b0de7fffabf1de6b63190af4c64c
Mikela Clemmons <glassresistor@gmail.com>
2017-04-17 13:59:48 -0700

    removed worker test and just checking for rabbitmq connection

--- intake/management/commands/smoke_test.py
+++ intake/management/commands/smoke_test.py
@@ -1,4 +1,5 @@
 import time
+from celery.task.control import inspect
 from django.db import connections
 from django.core import management
 from django.core.management.base import BaseCommand
@@ -11,16 +12,15 @@ class Command(BaseCommand):
         "This should fail is something is broken. Hopefully ;)")
 
     def handle(self, *args, **kwargs):
-        # Check db connect works
+        # Check db connection
         db_conn = connections['default']
         c = db_conn.cursor()  # If broken this will fail
-        # Check celery works exist
-        from celery.task.control import inspect
+        # Check rabbitmq connection
         insp = inspect()
         celery_worker_stats = insp.stats()
+        """
         if not celery_worker_stats:
             raise Exception("No Celery Workers")
-        """
         wsgi = Popen('./manage.py runserver', shell=True)
         time.sleep(3)
         management.call_command('test_urls_200', 'http://localhost:8000')
