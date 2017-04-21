f27025ea40936e6f41a950ceb3e53d590ecd5ee5
Mikela Clemmons <glassresistor@gmail.com>
2017-04-21 10:18:42 -0700

    removed smoke test from the release command
    removed smoke test mgmt command

--- intake/management/commands/heroku_release.py
+++ intake/management/commands/heroku_release.py
@@ -8,11 +8,9 @@ class Command(BaseCommand):
         "Sets up seeds based on what environment it runs in.")
 
     def handle(self, *args, **kwargs):
-        management.call_command('smoke_test')
         management.call_command('migrate')
         if getattr(settings, 'FLUSH_DATA', False):
             management.call_command('flush', interactive=False)
         management.call_command('load_essential_data')
         if getattr(settings, 'GENERATE_DUMMY_DATA', False):
             management.call_command('load_mock_data')
-        management.call_command('smoke_test')
