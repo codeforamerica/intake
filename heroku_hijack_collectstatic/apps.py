97d9dc98d7d1eebb036bed5dbc859c7282d6c4f6
Mikela Clemmons <glassresistor@gmail.com>
2017-04-26 12:17:21 -0700

    hijacked collectstatic and added compress

---
+++ heroku_hijack_collectstatic/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HerokuHijackCollectstaticConfig(AppConfig):
+    name = 'heroku_hijack_collectstatic'
---
+++ heroku_hijack_collectstatic/management/commands/collectstatic.py
@@ -0,0 +1,11 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.staticfiles.management.commands import collectstatic
+
+
+class Command(collectstatic.Command):
+
+    def handle(self, *args, **options):
+        call_command('compress',
+                engine="jinja2", extension=["jinja"], interactive=False)
+        super(Command, self).handle(*args, **options)
