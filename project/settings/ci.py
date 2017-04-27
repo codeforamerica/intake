3b37444400aa84bc42cec41653e1858318233c32
Mikela Clemmons <glassresistor@gmail.com>
2017-04-26 22:26:34 -0700

    removed whitenoise from heroku

--- project/settings/ci.py
+++ project/settings/ci.py
@@ -6,6 +6,8 @@ INTERNAL_IPS = ['127.0.0.1', '::1']
 CELERY_TASK_ALWAYS_EAGER = True
 
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+STATICFILES_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
 MEDIA_ROOT = os.path.join(REPO_DIR, 'project', 'media')
 
 BROWSER_STACK_ID = os.environ.get('BROWSER_STACK_ID')
