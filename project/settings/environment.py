76263c99f60ccf06bd03f40d6b5a1f2884cd8dd8
glassresistor <glassresistor@gmail.com>
2017-05-02 15:05:32 -0700

    Feature/build assets with compress (#763)
    * move less files, add first setting changes
    
    Migrated from gulp to compressor for less
    
    update less paths
    create jinja environment variable
    ./manage compress --engine jinja2 --extension jinja
    removed lessc from gulpfile.js
    
    * automated static assets
    
    moved img and voicemail to intake
    reorganized less so its in the intake app
    
    * all binary assets now in intake app
    
    moved images to intake app
    created symlink for fonts
    
    * fixed image page in less
    
    * now javascript is bundling with compressor
    
    added text/browserify precompiler
    moved js files to intake
    removed gulpfile
    updated templates to use js directly
    
    * removed frontend folder
    removed gulp
    
    * hijacked collectstatic and added compress
    
    * removed unnessary files and pep8
    
    * fixed order so compress run after collectstatic
    
    * added node to travis build
    
    * updated static path for ci and heroku
    
    * added secret key to travis
    
    * fixing envs in travis
    
    * oh travis treats each env lines as an env not a variable
    
    * made static files location global
    moved location
    updated less paths
    
    * moved staticfiles remove STATIC_ROOT from local_settings
    
    * updated links to imgages
    
    * removed whitenoise from heroku
    
    * set lessc location to node_moduels/.bin
    
    * dded some debug code to ls on travis
    
    * running ls before failing command
    
    * print settings on travis-ci
    
    * moved collectstatic to install portion to see if travis wipes files
    
    * added find for the js file
    
    * tried changing directory
    
    * turned on online compression for CI
    turned off manuall compress when doing online
    compression
    
    * went back to online compression since the issue is its not build
    staticfiles
    
    * split up collectstatic and compress for testing
    
    * printing static root
    
    * replaced the default media storage with
    'django.contrib.staticfiles.storage.StaticFilesStorage'
    
    * removed debug stuff from travis
    
    * added node modules path to less and symlink for fonts
    
    * pep8 fixs
    
    * updated herokus node_modules path
    
    * manually added fonts and remove the symlink to test heroku
    
    * changed the order of the buildpacks hoping it installs node first
    
    * ok works on heroku undoing testing stuff
    
    reverted back to using a symlink for fonts
    reverted back to a single node_module path for all
    
    * pep8 fixes
    
    * updated reference to images of people
    
    * fixed typo in browserify command
    
    * fixed paste into code
    
    * moved symlink back into repo
    
    * updated readme and makefile
    
    * added COMPRESS_OFFLINE to example local_settings
    
    * add integration tests for css
    
    * added stats feature checking

--- project/settings/environment.py
+++ project/settings/environment.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'something super secret')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # looks for 'DATABASE_URL' environmental variable
 DATABASES = {
@@ -21,10 +21,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('BUCKETEER_AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKETEER_BUCKET_NAME')
 
 MEDIA_ROOT = ''
-
-# static files location
-STATIC_ROOT = os.environ.get('STATIC_ROOT',
-                             os.path.join(REPO_DIR, 'project', 'static'))
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
