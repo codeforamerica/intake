5b4f43d0be9d5c94f8ee9a948359ed37597f0811
Ben Golder <bgolder@codeforamerica.org>
2017-05-15 14:12:54 -0700

    don't use analytics middleware on health check requests

--- intake/middleware.py
+++ intake/middleware.py
@@ -8,24 +8,34 @@ import intake.services.events_service as EventsService
 logger = logging.getLogger(__name__)
 
 
+def is_a_monitoring_request(request):
+    """Returns True it the input request is for monitoring or health check
+    purposes
+    """
+    # does the request begin with /health?
+    return request.get_full_path()[:7] == '/health'
+
+
 class PersistReferrerMiddleware:
 
     def process_request(self, request):
-        referrer = request.META.get('HTTP_REFERER')
-        if referrer:
-            referrer_host = urlparse(referrer).netloc
-            # make sure this is not an internal referrer
-            if referrer_host != request.get_host():
-                request.session['referrer'] = referrer
-        return None
+        if not is_a_monitoring_request(request):
+            referrer = request.META.get('HTTP_REFERER')
+            if referrer:
+                referrer_host = urlparse(referrer).netloc
+                # make sure this is not an internal referrer
+                if referrer_host != request.get_host():
+                    request.session['referrer'] = referrer
+            return None
 
 
 class PersistSourceMiddleware:
 
     def process_request(self, request):
-        source = request.GET.get('source')
-        if source:
-            request.session['source'] = source
+        if not is_a_monitoring_request(request):
+            source = request.GET.get('source')
+            if source:
+                request.session['source'] = source
 
 
 class GetCleanIpAddressMiddleware:
@@ -39,25 +49,26 @@ class GetCleanIpAddressMiddleware:
         return ip
 
     def process_request(self, request):
-        request.ip_address = self._get_client_ip(request)
-        return None
+        if not is_a_monitoring_request(request):
+            request.ip_address = self._get_client_ip(request)
+            return None
 
 
 class CountUniqueVisitorsMiddleware:
 
     def process_request(self, request):
-        visitor_id = request.session.get(
-            'visitor_id', None)
-        if not visitor_id:
-            visitor = Visitor(
-                referrer=request.session.get('referrer', ''),
-                source=request.session.get('source', ''),
-                ip_address=getattr(request, 'ip_address', '')
-            )
-            visitor.save()
-            EventsService.site_entered(visitor, request.get_full_path())
-            request.session['visitor_id'] = visitor.id
-        else:
-            visitor = Visitor.objects.get(id=visitor_id)
-            EventsService.page_viewed(visitor, request.get_full_path())
-        request.visitor = visitor
+        if not is_a_monitoring_request(request):
+            visitor_id = request.session.get('visitor_id', None)
+            if not visitor_id:
+                visitor = Visitor(
+                    referrer=request.session.get('referrer', ''),
+                    source=request.session.get('source', ''),
+                    ip_address=getattr(request, 'ip_address', '')
+                )
+                visitor.save()
+                EventsService.site_entered(visitor, request.get_full_path())
+                request.session['visitor_id'] = visitor.id
+            else:
+                visitor = Visitor.objects.get(id=visitor_id)
+                EventsService.page_viewed(visitor, request.get_full_path())
+            request.visitor = visitor
