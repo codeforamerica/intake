9eb3fafefb9493de551c896d60b44426547ba418
Mikela Clemmons <glassresistor@gmail.com>
2017-07-27 18:29:19 -0700

    preparing settings files for migration
    added new varialbes to prod.py
    moved Heroku specific settings into develop.py

--- project/settings/prod.py
+++ project/settings/prod.py
@@ -1,5 +1,15 @@
 from project.settings.environment import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'NAME': 'intake',
+        'USER': 'intake',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+    }
+}
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
