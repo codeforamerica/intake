48c7823799d786db195aa4daf51e97e6016a6e2e
Mark Rossetti <mark.a.rossetti@gmail.com>
2017-08-17 16:14:39 -0700

    Added custom command to migrate birthday from answers hash to dob column

---
+++ intake/management/commands/migrate_dob.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand
+from dateutil.parser import parse
+
+from intake import models
+
+
+class Command(BaseCommand):
+    help = "Fills dob column from answers['dob'] field"
+
+    def handle(self, *args, **options):
+        subs = models.FormSubmission.objects.filter(
+            dob__isnull=True)
+        migrated = 0
+        for sub in subs:
+            if not sub.dob:
+                dob_obj = sub.answers['dob']
+                parsed_dob = parse(dob_obj['year']+'-'+dob_obj['month']+'-'+dob_obj['day'])
+
+                sub.dob = parsed_dob
+                sub.save(update_fields=['dob'])
+                
+                migrated += 1
+        self.stdout.write(
+            self.style.SUCCESS(
+                "Updated dob on {} submissions".format(migrated))
+        )
