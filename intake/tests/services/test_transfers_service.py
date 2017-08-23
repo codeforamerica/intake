c16593a350728c282f4bee84fa118b6f430ac14f
Ben Golder <bgolder@codeforamerica.org>
2017-08-22 19:00:10 -0700

    adjust expected queries for easyaudit

--- intake/tests/services/test_transfers_service.py
+++ intake/tests/services/test_transfers_service.py
@@ -43,6 +43,6 @@ class TestTransferApplication(TestCase):
         to_org = Organization.objects.get(slug='ebclc')
         application = models.Application.objects.filter(
             organization__slug='a_pubdef').first()
-        with self.assertNumQueries(19):
+        with self.assertNumQueries(23):
             TransferService.transfer_application(
                 user, application, to_org, 'there was a temporal anomaly')
