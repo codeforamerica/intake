eecfe23c06653ba85ab559914f4e6b156ed6697d
Laura Kogler <lkogler@codeforamerica.org>
2017-10-03 14:25:23 -0700

    Order status types by new display_order field - Remove hard-coded primary key references to status types - Also fix intermittent error where generated DOB could be an invalid date
    Signed-off-by: Ben Golder <bgolder@codeforamerica.org>

--- intake/models/status_type.py
+++ intake/models/status_type.py
@@ -1,16 +1,6 @@
 from django.db import models
 from .template_option import TemplateOption, TemplateOptionManager
 
-# all of these are defined in the `template_options.json` fixture
-CANT_PROCEED = 1
-NO_CONVICTIONS = 2
-NOT_ELIGIBLE = 3
-ELIGIBLE = 4
-COURT_DATE = 5
-OUTCOME_GRANTED = 6
-OUTCOME_DENIED = 7
-TRANSFERRED = 8
-
 
 class PurgedStatusType(models.Model):
     """Placeholder for custom VIEW see intake migration 0067
