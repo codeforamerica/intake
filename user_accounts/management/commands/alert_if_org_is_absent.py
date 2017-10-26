09f7f8b479149fbc5f9bc7cb528e91fcb5ea2002
Ben Golder <bgolder@codeforamerica.org>
2017-10-26 12:12:50 -0700

    refactor query, add tests for command
    Signed-off-by: Laura Kogler <lkogler@codeforamerica.org>

--- user_accounts/management/commands/alert_if_org_is_absent.py
+++ user_accounts/management/commands/alert_if_org_is_absent.py
@@ -1,9 +1,11 @@
 from datetime import timedelta
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+from intake.models import Application
 from project.alerts import send_email_to_admins
 from user_accounts.models import Organization
 
@@ -16,26 +18,24 @@ class Command(BaseCommand):
         now = timezone.now()
         oldest_allowed_login_date = now - timedelta(days=20)
         for org in Organization.objects.all():
-            last_logins = User.objects.filter(
-                profile__organization__id=org.id
-            ).values_list('last_login', flat=True)
-            last_logins = [
-                login_datetime for login_datetime in last_logins
-                if login_datetime != None
-            ]
-            if last_logins:
-                latest_login = max(last_logins)
-                no_recent_logins = latest_login < oldest_allowed_login_date
+            latest_login = User.objects.filter(
+                profile__organization__id=org.id,
+                profile__organization__is_live=True,
+                last_login__isnull=False
+            ).order_by("-last_login").values_list(
+                'last_login', flat=True).first()
+
+            if latest_login and (latest_login < oldest_allowed_login_date):
                 latest_login_string = latest_login.strftime("%a %b %-d %Y")
-            else:
-                no_recent_logins = True
-                latest_login_string = 'forever'
-            unread_applications_count = Application.objects.filter(
+
+                unread_applications_count = Application.objects.filter(
                 has_been_opened=False, organization__id=org.id).count()
-            has_unread_applications = unread_applications_count > 0
-            msg = "{} has not logged in since {} and has {} unopened " \
-                  "applications".format(
-                org.name, latest_login_string, unread_applications_count)
-            if no_recent_logins and has_unread_applications:
-                send_email_to_admins(
-                    subject=msg, body="We should contact them")
+                subject = "Inactive organization on {}".format(
+                    settings.DEFAULT_HOST)
+                msg = "{} has not logged in since {} and has {} unopened " \
+                      "applications. We should contact them.".format(
+                        org.name, latest_login_string,
+                        unread_applications_count)
+                if unread_applications_count > 0:
+                    send_email_to_admins(
+                        subject=subject, message=msg)
