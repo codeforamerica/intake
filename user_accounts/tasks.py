a56571c4625325c999a5bb6b913e48c4e105ce10
Mikela Clemmons <glassresistor@gmail.com>
2017-11-01 11:59:02 -0700

    update new tasks to use zappa

--- user_accounts/tasks.py
+++ user_accounts/tasks.py
@@ -1,9 +1,10 @@
-from celery import shared_task
+from zappa.async import task
 
 
-@shared_task
+@task
 def create_mailgun_route(user_profile_id):
     from intake.services.mailgun_api_service import set_route_for_user_profile
     from user_accounts.models import UserProfile
+    # TODO: Fix whatever is causing circular imports.
     profile = UserProfile.objects.get(pk=user_profile_id)
     set_route_for_user_profile(profile)
