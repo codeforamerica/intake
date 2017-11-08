416f0b16616995382aa151eb1f9d3f99095975cf
Laura Kogler <lkogler@codeforamerica.org>
2017-11-08 14:28:18 -0800

    Make ALLOW_REQUEST_TO_MAILGUN setting track DIVERT_REMOTE_CONNECTIONS on prod and stage
    Signed-off-by: Mikela <mikela@codeforamerica.org>

--- project/decorators.py
+++ project/decorators.py
@@ -1,5 +1,10 @@
+import logging
 from functools import wraps
 from django.conf import settings
+
+
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
 
 
 def run_if_setting_true(setting_name, alternate_return_value):
@@ -7,6 +12,9 @@ def run_if_setting_true(setting_name, alternate_return_value):
         @wraps(func)
         def wrapped(*args, **kwargs):
             if not getattr(settings, setting_name, False):
+                logger.info(
+                    "{} caused skipped function call {}".
+                    format(setting_name, func.__name__))
                 return alternate_return_value
             return func(*args, **kwargs)
         return wrapped
