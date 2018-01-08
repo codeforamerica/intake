670e3fa764bde1c3611f4a32a17acd8d6dc928d9
Laura Kogler <lkogler@codeforamerica.org>
2018-01-08 10:32:11 -0800

    Roll back data export hack

--- email_csv/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EmailCsvConfig(AppConfig):
-    name = 'email_csv'
--- email_csv/urls.py
+++
@@ -1,8 +0,0 @@
-from url_robots.utils import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.email_csv,
-        name='email-csv', robots_allow=False),
-
-]
