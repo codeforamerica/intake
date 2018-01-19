c190ee943cd2b7edf4252227cab30c82931686b8
Mikela Clemmons <glassresistor@gmail.com>
2018-01-19 10:26:38 -0800

    added demo to autodeploy

--- update_builds_from_travis.sh
+++ update_builds_from_travis.sh
@@ -2,10 +2,11 @@ if [ "$TRAVIS_EVENT_TYPE" = "push" ]
 then
   if [ "$TRAVIS_BRANCH" = "develop" ]
   then
-    tower-cli job launch -J 10
+    tower-cli job launch -J 10 # staging
   fi
   if [ "$TRAVIS_BRANCH" = "master" ]
   then
-    tower-cli job launch -J 24
+    tower-cli job launch -J 25 # demo
+    tower-cli job launch -J 24 # production
   fi
 fi
