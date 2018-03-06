08de8d9ea8687b7027397a02397c2dc617c2759e
Paras Sanghavi <paras@codeforamerica.org>
2018-03-06 10:47:11 -0800

    Prevent user profiles from being deleted through the admin interface
    [#155713839]
    
    Signed-off-by: Laura Kogler <lkogler@codeforamerica.org>

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -16,4 +16,10 @@ class OrganizationAdmin(admin.ModelAdmin):
 
 @admin.register(models.UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    pass
+    def get_actions(self, request):
+        actions = super(UserProfileAdmin, self).get_actions(request)
+        del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
