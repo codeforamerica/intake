d8e07ece4f5f262ec552160a89a7d6ed15b4bd25
Christa Hartsock <christa@codeforamerica.org>
2020-03-02 20:52:47 -0800

    Fix style error
    Co-authored-by: Christa Hartsock <christa@codeforamerica.org>

--- project/heroku_wsgi.py
+++ project/heroku_wsgi.py
@@ -1,7 +1,6 @@
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.prod")
-
-from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
