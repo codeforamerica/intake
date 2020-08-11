85df0b48d28d2a55454ebf255f1028b9b161468e
Molly TM <mollyt@codeforamerica.org>
2020-08-11 08:46:10 -0700

    Remove unused `local.py` settings file
    - add explanatory comments to empty `development.py` settings file

---
+++ project/settings/development.py
@@ -0,0 +1,3 @@
+# This file intentionally left blank.
+# Developers maintain their own uncommitted `local_settings.py` files in the project root
+# For an example of a `local_settings.py` file, see `local_settings.py.example`
