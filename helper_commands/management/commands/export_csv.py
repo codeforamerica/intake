e99cc7bdbc29976b1aee36b5ac561f87433f2f33
Christa Hartsock <christa@codeforamerica.org>
2020-10-09 15:07:16 -0700

    Update bucket var name in script

--- helper_commands/management/commands/export_csv.py
+++ helper_commands/management/commands/export_csv.py
@@ -97,7 +97,7 @@ class Command(BaseCommand):
                     writer.writerow(columns)
             conn = S3Connection(settings.AWS_ACCESS_KEY_ID,
                                 settings.AWS_SECRET_ACCESS_KEY)
-            media_bucket = conn.get_bucket(settings.MEDIA_BUCKET)
+            media_bucket = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
             key = Key(media_bucket)
             key.key = 'database_export.csv'
             key.set_contents_from_filename(filename)
