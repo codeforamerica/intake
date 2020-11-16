c7cc7cb801dc176eaa84e9cd0100d76fd2c7e53f
Molly TM <mollyt@codeforamerica.org>
2020-11-16 11:31:06 -0800

    Remove unused code for Browserstack
    - left behind in test code but not used

--- features/environment.py
+++ features/environment.py
@@ -1,11 +1,7 @@
-from browserstack.local import Local
 from django.conf import settings
 from django.core.management import call_command
 from selenium import webdriver
 
-
-USERNAME = settings.BROWSER_STACK_ID
-ACCESS_KEY = settings.BROWSER_STACK_KEY
 
 # -- FILE: features/environment.py
 # USE: behave -D BEHAVE_DEBUG_ON_ERROR         (to enable  debug-on-error)
@@ -20,38 +16,7 @@ def setup_debug_on_error(userdata):
     BEHAVE_DEBUG_ON_ERROR = userdata.getbool("BEHAVE_DEBUG_ON_ERROR")
 
 
-def start_local():
-    """Code to start browserstack local before start of test."""
-    global bs_local
-    bs_local = Local()
-    bs_local_args = {"key": ACCESS_KEY, "forcelocal": "true"}
-    bs_local.start(**bs_local_args)
-
-
-def stop_local():
-    """Code to stop browserstack local after end of test."""
-    global bs_local
-    if bs_local is not None:
-        bs_local.stop()
-
-
 def before_all(context):
-    # start_local()
-    desired_cap = {
-        'browser': 'Chrome',
-        'browser_version': '57.0',
-        'os': 'Windows',
-        'os_version': '7',
-        'resolution': '1024x768',
-    }
-    desired_capabilities = desired_cap
-    desired_capabilities['browserstack.local'] = True
-    desired_capabilities['browserstack.debug'] = True
-    # url = 'http://%s:%s@hub.browserstack.com:80/wd/hub'
-    # context.browser = webdriver.Remote(
-    #     desired_capabilities=desired_capabilities,
-    #     command_executor=url % (USERNAME, ACCESS_KEY)
-    # )
     options = webdriver.ChromeOptions()
     options.add_argument('headless')
 
@@ -71,7 +36,6 @@ def django_ready(context):
 
 def after_all(context):
     context.browser.quit()
-    # stop_local()
 
 
 def before_scenario(context, scenario):
